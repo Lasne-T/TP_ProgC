06f8105ea975af036063266574c7abfd362dce40
Lasne-T <thibaudlasne16@gmail.com>
2025-04-16 15:31:14 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int compteur = 5; 
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i % 2 == 0 && j > 1 && j < i) {
+                printf("# ");
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
 
+    return 0;
+}
