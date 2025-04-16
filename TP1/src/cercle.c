e35087dfba7d39535c8bc00873f395b9e8e7d96c
Lasne-T <thibaudlasne16@gmail.com>
2025-04-16 14:33:39 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -5,8 +5,8 @@ int main() {
     float rayon;
     printf("Entrez la valeur du rayon");
     scanf("%f", &rayon);
-    float air = Math_PI * rayon * rayon;
-    float perimeter = 2 * Math_PI * rayon;
+    float air = M_PI * rayon * rayon;
+    float perimeter = 2 * M_PI * rayon;
     printf("Surface de l'air : %f\n", air);
     printf("Perimetre : %f\n", perimeter);
     return 0;
