d68f2b03e75fe9f145abb658646a4fd7d3a9a178
Lasne-T <thibaudlasne16@gmail.com>
2025-04-16 15:35:14 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    int compteur = 5; 
+    int compteur ;
+    printf("Taille de la pyramide :");
+    scanf("%d",&compteur);
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
             if (i % 2 == 0 && j > 1 && j < i) {
