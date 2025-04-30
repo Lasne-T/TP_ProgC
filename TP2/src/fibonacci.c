134116cf7d5d6d0289ee054d6ca624022e761cda
Lasne-T <thibaudlasne16@gmail.com>
2025-04-30 15:28:02 +0200

    Update fibonacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    printf("Entrez le nombre de termes de Fibonacci à afficher : ");
+    scanf("%d", &n);
+    
+    if (n <= 0) {
+        printf("Veuillez entrer un nombre positif.\n");
+        return 1;
+    }
+
+
+    int U0 = 0, U1 = 1, Un;
+
+    printf("Suite de Fibonacci jusqu'à U%d : ", n);
+
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            printf("%d", U0);
+        } else if (i == 1) {
+            printf(", %d", U1);
+        } else {
+            Un = U0 + U1;
+            printf(", %d", Un);
+            U0 = U1;
+            U1 = Un;
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
