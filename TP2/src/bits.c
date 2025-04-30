1376d3f28da9cc6fcbb6110fe45eee9309a4ed7c
Lasne-T <thibaudlasne16@gmail.com>
2025-04-30 15:25:08 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+
 int main() {
-    unsigned int d = 0x80100008; //0x00100008
-    unsigned int masque4 = 1 << (32 - 4);
-    unsigned int masque20 = 1 << (32 - 20);
-  
-    if ((d & masque4) && (d & masque20)) {
+    unsigned int d = 0x08801000 | (1 << 12);
+
+    
+    int bit1 = (d >> (31 - 3)) & 1;  
+    int bit2 = (d >> (31 - 19)) & 1; 
+
+    if (bit1 == 1 && bit2 == 1) {
         printf("1\n");
     } else {
         printf("0\n");
