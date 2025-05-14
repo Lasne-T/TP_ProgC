4ee547580061e5f63118ac944ff4af90885338c5
Lasne-T <thibaudlasne16@gmail.com>
2025-05-14 17:27:35 +0200

    Update client.c

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -5,50 +5,35 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
 #include "client.h"
 
 int envoie_recois_message(int socketfd)
 {
   char data[1024];
-
   memset(data, 0, sizeof(data));
-
-  char message[1024];
-  printf("Votre message (max 1000 caractères): ");
-  fgets(message, sizeof(message), stdin);
-
-  strcpy(data, "message: ");
-  strcat(data, message);
-
+  const char *message = "message: Bonjour, serveur!";
+  strcpy(data, message);
   int write_status = write(socketfd, data, strlen(data));
   if (write_status < 0)
   {
     perror("Erreur d'écriture");
     return -1;
   }
-
   memset(data, 0, sizeof(data));
-
   int read_status = read(socketfd, data, sizeof(data));
   if (read_status < 0)
   {
     perror("Erreur de lecture");
     return -1;
   }
-
   printf("Message reçu: %s\n", data);
-
-  return 0; 
+  return 0;
 }
 
 int main()
 {
   int socketfd;
-
   struct sockaddr_in server_addr;
-
- 
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
   if (socketfd < 0)
   {
@@ -59,11 +44,7 @@ int main()
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
-
-  if (inet_pton(AF_INET, "10.0.48.4", &server_addr.sin_addr) <= 0) {
-    perror("Adresse IP invalide");
-    exit(EXIT_FAILURE);
-  }
+  server_addr.sin_addr.s_addr = INADDR_ANY;
 
   int connect_status = connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
   if (connect_status < 0)
@@ -74,7 +55,6 @@ int main()
 
   while (1)
   {
-    
     envoie_recois_message(socketfd);
   }
 
