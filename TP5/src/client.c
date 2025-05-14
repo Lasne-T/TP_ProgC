1691dfd784b3b0597fc0bfe78fc59243ac94fe01
Lasne-T <thibaudlasne16@gmail.com>
2025-05-14 16:47:58 +0200

    Update client.c

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -1,10 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2021 John Samuel
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- *
- */
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,32 +5,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
 #include "client.h"
 
-/**
- * Fonction pour envoyer et recevoir un message depuis un client connecté à la socket.
- *
- * @param socketfd Le descripteur de la socket utilisée pour la communication.
- * @return 0 en cas de succès, -1 en cas d'erreur.
- */
 int envoie_recois_message(int socketfd)
 {
   char data[1024];
-
-  // Réinitialisation de l'ensemble des données
   memset(data, 0, sizeof(data));
 
-  // Demande à l'utilisateur d'entrer un message
   char message[1024];
   printf("Votre message (max 1000 caractères): ");
   fgets(message, sizeof(message), stdin);
 
-  // Construit le message avec une étiquette "message: "
   strcpy(data, "message: ");
   strcat(data, message);
 
-  // Envoie le message au client
   int write_status = write(socketfd, data, strlen(data));
   if (write_status < 0)
   {
@@ -45,10 +26,7 @@ int envoie_recois_message(int socketfd)
     return -1;
   }
 
-  // Réinitialisation de l'ensemble des données
   memset(data, 0, sizeof(data));
-
-  // Lit les données de la socket
   int read_status = read(socketfd, data, sizeof(data));
   if (read_status < 0)
   {
@@ -56,21 +34,15 @@ int envoie_recois_message(int socketfd)
     return -1;
   }
 
-  // Affiche le message reçu du client
   printf("Message reçu: %s\n", data);
-
-  return 0; // Succès
+  return 0;
 }
 
 int main()
 {
   int socketfd;
-
   struct sockaddr_in server_addr;
 
-  /*
-   * Creation d'une socket
-   */
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
   if (socketfd < 0)
   {
@@ -78,15 +50,12 @@ int main()
     exit(EXIT_FAILURE);
   }
 
-  // détails du serveur (adresse et port)
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
   server_addr.sin_addr.s_addr = INADDR_ANY;
 
-  // demande de connection au serveur
-  int connect_status = connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (connect_status < 0)
+  if (connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
   {
     perror("connection serveur");
     exit(EXIT_FAILURE);
@@ -94,9 +63,9 @@ int main()
 
   while (1)
   {
-    // appeler la fonction pour envoyer un message au serveur
     envoie_recois_message(socketfd);
   }
 
   close(socketfd);
+  return 0;
 }
